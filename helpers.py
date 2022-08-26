582c889e3d0e2187e0ac79b6297e612776c37622
MUHAMMEDFAISALKC <87296701+MUHAMMEDFAISALKC@users.noreply.github.com>
2022-08-26 18:03:17 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,9 @@
+import imp
 from flask import redirect, session
 from functools import  wraps
+import os
+import qrcode
+
 
 
 def login_required(f):
@@ -10,3 +14,9 @@ def login_required(f):
         return f(*args, **kwargs) 
     return decorated
 
+def qrcodemaking(amount):
+    qlink= "upi://pay?appid=com.infra.inb&tr=&mc=&pa=9447536992@indianbank&pn=MS%GABBRO%CEMENT%BRICKS%AND%PRODUCTS&tn=&am="+amount+"&cu=INR"
+    img = qrcode.make(qlink)
+    print(qlink)
+    img.save("./static/img/qr.png", "PNG")
+    os.system("open qr.png")
